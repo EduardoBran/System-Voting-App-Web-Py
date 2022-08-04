75f3190d7e4075ff0814d1485cf43e7b9e11b9db
Eduardo Brandao <eduardo.ads1814@gmail.com>
2022-08-04 16:54:41 -0300

    add rota para as paginas criadas details e results

--- votacao/views.py
+++ votacao/views.py
@@ -8,4 +8,16 @@ def index(request):
     
     context = {'latest_question_list': latest_question_list}
     return render(request, 'votacao/index.html', context)
-    
+
+
+def detail(request, question_id):
+    question = Question.objects.get(pk=question_id)
+    context = {'question': question}
+    return render(request, 'votacao/detail.html', context)
+
+
+
+def results(request, question_id):
+    question = Question.objects.get(pk=question_id)
+    context = {'question': question}
+    return render(request, 'votacao/results.html', context)
