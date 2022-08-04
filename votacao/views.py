0b1ed44fb7e963c0904e7c1412ab4e9b766768b9
Eduardo Brandao <eduardo.ads1814@gmail.com>
2022-08-04 12:17:03 -0300

    criado template temporario para index | exibindo enquetes

--- votacao/views.py
+++ votacao/views.py
@@ -4,6 +4,8 @@ from .models import *
 
 
 def index(request):
-    context = {}
+    latest_question_list = Question.objects.order_by('-pub_date')
+    
+    context = {'latest_question_list': latest_question_list}
     return render(request, 'votacao/index.html', context)
     
