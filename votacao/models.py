968d248bf8e96446320d93c3bb3590f26694de71
Eduardo Brandao <eduardo.ads1814@gmail.com>
2022-08-04 11:52:22 -0300

    criado o models

--- votacao/models.py
+++ votacao/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200, verbose_name='Pergunta')
+    pub_date = models.DateTimeField(default=timezone.now, verbose_name='Data da publicação')
+    show = models.BooleanField(default=True, verbose_name='Mostrar')    
+    
+    def __str__(self):
+        return self.question_text
+    
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200, verbose_name='Opções')
+    votes = models.IntegerField(default=0, verbose_name='Votos')
+    
+    def __str__(self):
+        return self.choice_text
+    
+
+
