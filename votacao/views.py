c09166ac249596542471d04703a7684bc426e6c9
Eduardo Brandao <eduardo.ads1814@gmail.com>
2022-08-05 19:29:23 -0300

    add logic vote | edite view vote | edite url vote

--- votacao/views.py
+++ votacao/views.py
@@ -1,5 +1,6 @@
 from django.http import Http404, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
 
 from .models import *
 
@@ -21,8 +22,24 @@ def detail(request, question_id):
     return render(request, 'votacao/detail.html', context)
 
 
-
 def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     context = {'question': question}
     return render(request, 'votacao/results.html', context)
+
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+        print(selected_choice)
+        
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'votacao/detail.html', {'question': question, 'error_message': "Você não selecionou nenhuma opção.",})
+    
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        
+        return HttpResponseRedirect(reverse('votacao:results', args=(question.id, )))    
