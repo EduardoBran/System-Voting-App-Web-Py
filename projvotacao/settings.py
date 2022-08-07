e808216cba9b02eb5eaa10808469e2c27b524631
Eduardo Brandao <eduardo.ads1814@gmail.com>
2022-08-07 18:49:37 -0300

    install crispy_forms | edit layout page addPolls

--- projvotacao/settings.py
+++ projvotacao/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'votacao',
     'main',
     'perfil',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -130,6 +131,8 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'templates/static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- votacao/models.py
+++ votacao/models.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200, verbose_name='Pergunta')
-    pub_date = models.DateTimeField(default=timezone.now, verbose_name='Data da publicação')
+    pub_date = models.DateTimeField(default=timezone.now, verbose_name='Data')
     show = models.BooleanField(default=True, verbose_name='Mostrar')    
     
     def __str__(self):
@@ -13,7 +13,7 @@ class Question(models.Model):
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200, verbose_name='Opções')
+    choice_text = models.CharField(max_length=200, verbose_name='Opção')
     votes = models.IntegerField(default=0, verbose_name='Votos')
     
     def __str__(self):
